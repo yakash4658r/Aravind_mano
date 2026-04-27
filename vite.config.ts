81ce7fd9a2c4cc012e2e4fe84895f766b9c286c3
yakash4658r <yakashelavarasan@outlook.com>
2026-04-27 18:27:17 +0530

    Configure GitHub Pages deployment via Actions

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Aravind_mano/",
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
